c81ddea86c971c1a83d52e832ac13fce73104d45
Mateusz <mateuszszczykutowicz@wp.pl>
2020-10-23 18:40:24 +0200

    Structure

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./app.js",
+  output: {
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "./build/js"),
+  },
+  watch: false,
+  mode: "development",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"), // Prefer `dart-sass`
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
